ec25d9f80450d8ff57dfcfce44d4721dda283920
summerFreeze6019 <summerFreeze6019@protonmail.com>
2024-05-17 17:10:49 +0300

    Improve accessibility

---
+++ gee/gee.go
@@ -0,0 +1,48 @@
+package gee
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	log.Print("Route %4s - %s ", method, pattern)
+	engine.router[key] = handler
+}
+
+// GET method request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST method request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run to method
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprint(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+
+}
